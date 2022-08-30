26cffc3e3d0aa40c6d4ece93cc40404812426b4c
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-08-30 14:24:48 +0200

    if error fixed

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const todos = ["Collect Chicken Eggs", "Clean Litter Box"];
 
 while (input !== "quit" && input !== "q") {
   if (input === "list") {
+    console.log("*****************");
     for (let i = 0; i < todos.length; i++) {
-      console.log("*****************");
       console.log(`${i}: ${todos[i]}`);
-      console.log("*****************");
     }
+    console.log("*****************");
+  } else if (input === "new") {
+    const newTodo = prompt("Ok, what is the new todo?");
+    todos.push(newTodo);
+    console.log(`${newTodo} added to a list!`);
   }
+
   input = prompt("What would you like to do?");
 }
 
